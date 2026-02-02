8fe4484a406d91d3806caa17f76a4a9b45d6b606
Edwin Contreras <ed25gonz@gmail.com>
2026-02-01 20:40:25 -0400

    feat: Initialize Unit Convert project with basic HTML, JavaScript, and Tailwind CSS.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import '@/style.css'
+// Supports weights 100-800
+import '@fontsource-variable/ubuntu-sans';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1 class="text-3xl">Unit Convert</h1>
+  </div>
+`
